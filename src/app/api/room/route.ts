f961fbc71e7936839a4d4c1fd28c2e75858272a9
riku546 <opa9788@gmail.com>
2024-09-22 18:12:55 +0900

    roomApiのpostを作成

---
+++ src/app/api/room/route.ts
@@ -0,0 +1,32 @@
+import { dbConnect } from '@/lib/dbConnect';
+import { handleAPIError } from '@/lib/handleAPIError';
+import { prisma } from '@/lib/prisma';
+import { apiRes } from '@/lib/types';
+import { RoomStatus } from '@prisma/client';
+import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
+
+const POST = async (req: Request, res: NextResponse) =>
+  handleAPIError(async () => {
+    await dbConnect();
+    const { name, userId, category, difficulty, anserTimeLimit, thinkingTimeLimit } =
+      await req.json();
+    const inviteId: string = uuidv4();
+    const newRoom = await prisma.room.create({
+      data: {
+        name,
+        category,
+        difficulty,
+        anserTimeLimit,
+        thinkingTimeLimit,
+        inviteId,
+        status: RoomStatus.WAITING,
+        users: { connect: { id: userId } },
+      },
+    });
+
+    return NextResponse.json<apiRes>({
+      message: 'room created',
+      data: { inviteId: inviteId, newRoom: newRoom },
+    });
+  });
