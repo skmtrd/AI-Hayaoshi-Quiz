d2c9848359494b481ff2ccf693af41d3cace8080
skmtrd <skmtgame23@gmail.com>
2024-09-23 23:17:08 +0900

    feat: can join room

--- src/components/element/RoomList.tsx
+++ src/components/element/RoomList.tsx
@@ -2,10 +2,33 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { RoomSchema } from '@/lib/schemas';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
+const joinRoom = async (roomId: string) => {
+  const res = await fetch(`/api/room/${roomId}/join`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
+};
+
 type RoomType = z.infer<typeof RoomSchema>;
+
 const RoomList = ({ rooms }: { rooms: RoomType[] }) => {
+  const router = useRouter();
+  const handleJoinRoom = async (roomId: string) => {
+    const res = await joinRoom(roomId);
+    if (res.message === 'join success') {
+      router.push(`/room/${res.data.newRoom.id}`);
+    } else if (res.message === 'room is full') {
+      toast.error('部屋が満員です');
+    }
+  };
+
   return (
     <ScrollArea className='h-[calc(100vh-220px)] w-full'>
       {rooms.map((room) => (
@@ -15,7 +38,9 @@ const RoomList = ({ rooms }: { rooms: RoomType[] }) => {
           </CardHeader>
           <CardContent>
             <p className='text-sm text-muted-foreground'>参加者:{room.numberOfUser}/10人</p>
-            <Button className='mt-2 w-full text-white'>参加する</Button>
+            <Button className='mt-2 w-full text-white' onClick={() => handleJoinRoom(room.id)}>
+              参加する
+            </Button>
           </CardContent>
         </Card>
       ))}
