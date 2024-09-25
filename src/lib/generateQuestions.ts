42f33206f6a645aa8ec35f54076cdc3be5537497
skmtrd <skmtgame23@gmail.com>
2024-09-25 11:34:20 +0900

    feat: room create with AI question

--- src/lib/generateQuestions.ts
+++ src/lib/generateQuestions.ts
@@ -3,14 +3,16 @@ import { OpenAI } from 'openai';
 import { zodResponseFormat } from 'openai/helpers/zod';
 import { z } from 'zod';
 
-const responseSchema = z.array(
-  z.object({
-    question: z.string(),
-    correctAnswer: z.string(),
-    incorrectAnswers: z.array(z.string()),
-    comment: z.string(),
-  }),
-);
+const responseSchema = z.object({
+  quizzes: z.array(
+    z.object({
+      question: z.string(),
+      correctAnswer: z.string(),
+      incorrectAnswers: z.array(z.string()),
+      comment: z.string(),
+    }),
+  ),
+});
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -33,20 +35,30 @@ export const generateQuestions = async (theme: string, difficulty: number) => {
   (例えば:サムスクリューなどは、日本語では一般的に知られていないため、選択肢にはなりません)
 
   例:
-  質問文: 日本の首都はどこですか？
-  正しい答え: 東京
-  間違った答え1: 大阪
-  間違った答え2: 京都
-  間違った答え3: 札幌
+クイズの形式は以下の通りです。必ずJSON形式でオブジェクトとして返答してください。
+{
+  "quizzes": [
+    {
+      "question": "質問文",
+      "correctAnswer": "正しい答え",
+      "incorrectAnswers": ["間違った答え1", "間違った答え2", "間違った答え3"],
+      "comment": "簡潔な解説"
+    },
+    ...
+  ]
+}
 
   簡潔な解説: 日本の首都は東京です。
 
   それでは、クイズを作成してください。
   `;
   const response = await client.beta.chat.completions.parse({
-    model: 'gpt-4o-2024-08-06',
+    model: 'gpt-4o-mini',
     messages: [{ role: 'system', content: prompt }],
     response_format: zodResponseFormat(responseSchema, 'quiz'),
   });
-  return response.choices[0].message.parsed;
+  if (!response.choices[0].message.parsed) {
+    return null;
+  }
+  return response.choices[0].message.parsed.quizzes;
 };
