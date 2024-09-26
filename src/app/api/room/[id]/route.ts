f608b0394e85bf67707148831159f74317267152
caru-ini <caru17122@gmail.com>
2024-09-26 13:03:48 +0000

    refactor: remove unnecessary route

--- src/app/api/room/[id]/route.ts
+++ src/app/api/room/[id]/route.ts
@@ -4,19 +4,6 @@ import { prisma } from '@/lib/prisma';
 import { apiRes } from '@/lib/types';
 import { NextResponse } from 'next/server';
 
-//部屋への参加api
-export const PUT = async (req: Request, res: NextResponse) =>
-  handleAPIError(async () => {
-    await dbConnect();
-    const { roomId, userId } = await req.json();
-    const numberOfUser = await prisma.room.findUnique({
-      where: {
-        id: roomId,
-      },
-    });
-    return NextResponse.json<apiRes>({ message: 'join success' });
-  });
-
 //部屋の情報取得api
 export const GET = async (
   req: Request,
