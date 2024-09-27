7178657770ceca7043f151d1ba1e426fb2897ee1
caru-ini <caru17122@gmail.com>
2024-09-27 14:00:42 +0000

    fix: update file name to valid name

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth';
-import { WaitingScreen } from '@/components/element/waitingScreen';
+import { WaitingScreen } from '@/components/element/WaitingScreen';
 import { prisma } from '@/lib/prisma';
 import { redirect } from 'next/navigation';
 
