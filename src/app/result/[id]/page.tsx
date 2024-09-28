15b097d350986d2f2548be7414163d03b802cff4
caru-ini <caru17122@gmail.com>
2024-09-28 11:02:37 +0000

    feat: update styles

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -3,6 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { prisma } from '@/lib/prisma';
 import { cn } from '@/lib/utils';
 import { ArrowLeft, Crown } from 'lucide-react';
@@ -42,49 +43,53 @@ export default async function ResultPage({ params }: { params: { id: string } })
           <p className='text-xl'>テーマ：{room.theme}</p>
         </CardHeader>
         <CardContent>
-          <div className='space-y-4'>
-            {room.Result.map((result) => (
-              <Link href={`/user/${result.user.id}`} key={result.id} className='block'>
-                <div className='flex items-center justify-between border-b pb-4'>
-                  <div className='flex items-center'>
-                    <Avatar className='mr-3'>
-                      <AvatarImage src={result.user.image ?? ''} alt={result.user.name ?? ''} />
-                      <AvatarFallback>
-                        {result.user.name?.slice(0, 2).toUpperCase() ?? 'U'}
-                      </AvatarFallback>
-                    </Avatar>
-                    <div>
-                      <h3 className='flex items-center text-lg font-semibold'>
-                        {result.user.name}
-                        {result.correctCount === highestScore && (
-                          <Crown className='ml-2 size-5 text-yellow-500' />
-                        )}
-                      </h3>
-                      <p className='text-sm text-muted-foreground'>正解数: {result.correctCount}</p>
+          <ScrollArea className='max-h-[300px] pr-4'>
+            <div className='space-y-4'>
+              {room.Result.map((result) => (
+                <Link href={`/user/${result.user.id}`} key={result.id} className='block'>
+                  <div className='flex items-center justify-between border-b pb-4'>
+                    <div className='flex items-center'>
+                      <Avatar className='mr-3'>
+                        <AvatarImage src={result.user.image ?? ''} alt={result.user.name ?? ''} />
+                        <AvatarFallback>
+                          {result.user.name?.slice(0, 2).toUpperCase() ?? 'U'}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <h3 className='flex items-center text-lg font-semibold'>
+                          {result.user.name}
+                          {result.correctCount === highestScore && (
+                            <Crown className='ml-2 size-5 text-yellow-500' />
+                          )}
+                        </h3>
+                        <p className='text-sm text-muted-foreground'>
+                          正解数: {result.correctCount}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <Badge
-                    className={cn(
-                      result.ratingDelta &&
+                    <Badge
+                      className={cn(
+                        result.ratingDelta &&
+                          (result.ratingDelta > 0
+                            ? 'bg-green-500 hover:bg-green-600'
+                            : result.ratingDelta < 0
+                              ? 'bg-red-500 hover:bg-red-600'
+                              : 'bg-gray-500 hover:bg-gray-600'),
+                        'text-white',
+                      )}
+                    >
+                      {result.ratingDelta &&
                         (result.ratingDelta > 0
-                          ? 'bg-green-500 hover:bg-green-600'
+                          ? `+${result.ratingDelta}`
                           : result.ratingDelta < 0
-                            ? 'bg-red-500 hover:bg-red-600'
-                            : 'bg-gray-500 hover:bg-gray-600'),
-                      'text-white',
-                    )}
-                  >
-                    {result.ratingDelta &&
-                      (result.ratingDelta > 0
-                        ? `+${result.ratingDelta}`
-                        : result.ratingDelta < 0
-                          ? result.ratingDelta
-                          : '±0')}
-                  </Badge>
-                </div>
-              </Link>
-            ))}
-          </div>
+                            ? result.ratingDelta
+                            : '±0')}
+                    </Badge>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </ScrollArea>
         </CardContent>
         <CardFooter className='flex w-full justify-between'>
           <Link href='/room' passHref className='mr-2 flex-1'>
@@ -94,7 +99,7 @@ export default async function ResultPage({ params }: { params: { id: string } })
             </Button>
           </Link>
           <div className='ml-2 flex-1'>
-            <ShareModal roomId={room.id} />
+            <ShareModal path={`result/${room.id}`} text='試合結果をシェア' />
           </div>
         </CardFooter>
       </Card>
--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,7 +1,19 @@
+import { ShareModal } from '@/components/element/ShareModal';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { prisma } from '@/lib/prisma';
 import { cn } from '@/lib/utils';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 async function getUser(id: string) {
@@ -37,7 +49,7 @@ const UserPage = async ({ params }: { params: { id: string } }) => {
       : 0;
 
   return (
-    <div className='mx-auto flex min-h-screen w-full max-w-6xl flex-col items-center justify-center gap-y-10 px-4'>
+    <div className='mx-auto flex w-full max-w-6xl flex-1 flex-col items-center justify-center gap-y-10 px-4'>
       <Card className='max-h-full w-full max-w-2xl'>
         <CardHeader>
           <div className='flex items-center'>
@@ -54,11 +66,11 @@ const UserPage = async ({ params }: { params: { id: string } }) => {
         <CardContent>
           <div className='mb-6 grid grid-cols-2 gap-4'>
             <p>
-              <span className='font-semibold text-gray-600'>レーティング:</span>{' '}
+              <span className='font-semibold text-gray-600'>レーティング:</span>
               <span className='text-lg'>{user.rating}</span>
             </p>
             <p>
-              <span className='font-semibold text-gray-600'>勝率:</span>{' '}
+              <span className='font-semibold text-gray-600'>勝率:</span>
               <span
                 className={cn(
                   'text-lg font-semibold',
@@ -73,13 +85,13 @@ const UserPage = async ({ params }: { params: { id: string } }) => {
               </span>
             </p>
             <p>
-              <span className='font-semibold text-gray-600'>作成日:</span>{' '}
+              <span className='font-semibold text-gray-600'>作成日:</span>
               <span className='text-sm'>
                 {new Date(user.createdAt).toLocaleDateString('ja-JP')}
               </span>
             </p>
             <p>
-              <span className='font-semibold text-gray-600'>更新日:</span>{' '}
+              <span className='font-semibold text-gray-600'>更新日:</span>
               <span className='text-sm'>
                 {new Date(user.updatedAt).toLocaleDateString('ja-JP')}
               </span>
@@ -93,51 +105,66 @@ const UserPage = async ({ params }: { params: { id: string } }) => {
                   ({user.results.length}件)
                 </span>
               </h2>
-              <ul className='max-h-96 space-y-4 overflow-y-auto'>
-                {user.results.map((result) => (
-                  <li
-                    key={result.id}
-                    className={cn(
-                      result.ratingDelta
-                        ? result.ratingDelta > 0
-                          ? 'bg-green-50'
-                          : 'bg-red-50'
-                        : 'bg-gray-50',
-                      'rounded-lg border border-gray-200 p-3 shadow-sm',
-                    )}
-                  >
-                    <p className='mb-2 text-lg font-medium'>{result.room.theme}</p>
-                    <div className='flex items-center justify-between'>
-                      <p>
-                        <span className='font-semibold text-muted-foreground'>
-                          レーティング変動:
-                        </span>{' '}
-                        <span
-                          className={cn(
-                            'font-bold',
-                            result.ratingDelta
-                              ? result.ratingDelta > 0
-                                ? 'text-green-600'
-                                : 'text-red-600'
-                              : 'text-muted-foreground',
-                          )}
-                        >
-                          {result.ratingDelta && result.ratingDelta > 0 ? '+' : ''}
-                          {result.ratingDelta}
-                        </span>
-                      </p>
-                      <p className='text-sm text-muted-foreground'>
-                        {new Date(result.createdAt).toLocaleString('ja-JP')}
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              <ScrollArea className='max-h-[400px]'>
+                <ul className='flex flex-col gap-y-4'>
+                  {user.results.map((result) => (
+                    <Link href={`/result/${result.room.id}`} key={result.id} passHref>
+                      <li
+                        key={result.id}
+                        className={cn(
+                          result.ratingDelta
+                            ? result.ratingDelta > 0
+                              ? 'bg-green-50'
+                              : 'bg-red-50'
+                            : 'bg-gray-50',
+                          'rounded-lg border border-gray-200 p-3 shadow-sm',
+                        )}
+                      >
+                        <p className='mb-2 text-lg font-medium'>{result.room.theme}</p>
+                        <div className='flex items-center justify-between'>
+                          <p>
+                            <span className='font-semibold text-muted-foreground'>
+                              レーティング変動:
+                            </span>
+                            <span
+                              className={cn(
+                                'font-bold',
+                                result.ratingDelta
+                                  ? result.ratingDelta > 0
+                                    ? 'text-green-600'
+                                    : 'text-red-600'
+                                  : 'text-muted-foreground',
+                              )}
+                            >
+                              {result.ratingDelta && result.ratingDelta > 0 ? '+' : ''}
+                              {result.ratingDelta}
+                            </span>
+                          </p>
+                          <p className='text-sm text-muted-foreground'>
+                            {new Date(result.createdAt).toLocaleString('ja-JP')}
+                          </p>
+                        </div>
+                      </li>
+                    </Link>
+                  ))}
+                </ul>
+              </ScrollArea>
             </div>
           ) : (
             <p className='italic text-muted-foreground'>まだマッチ履歴がありません。</p>
           )}
         </CardContent>
+        <CardFooter className='flex w-full justify-between'>
+          <Link href='/room' passHref className='mr-2 flex-1'>
+            <Button className='w-full' variant='secondary'>
+              <ArrowLeft className='mr-2' />
+              部屋一覧に戻る
+            </Button>
+          </Link>
+          <div className='ml-2 flex-1'>
+            <ShareModal path={`user/${user.id}`} text='ユーザーをシェア' />
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
