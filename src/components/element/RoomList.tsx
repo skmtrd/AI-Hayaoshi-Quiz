3b8518c66c6fcbb9355e878b59e0be5bedfd974e
skmtrd <skmtgame23@gmail.com>
2024-09-28 23:20:03 +0900

    fix: roomlist

--- src/components/element/RoomList.tsx
+++ src/components/element/RoomList.tsx
@@ -5,16 +5,40 @@ import { RoomSchema } from '@/lib/schemas';
 import { formatDistanceToNow } from 'date-fns';
 import { ja } from 'date-fns/locale';
 import { Clock, Users } from 'lucide-react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 interface RoomListProps {
   rooms: z.infer<typeof RoomSchema>[];
 }
 
+const joinRoom = async (roomId: string) => {
+  const res = await fetch(`/api/room/${roomId}/join`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return res.json();
+};
+type RoomType = z.infer<typeof RoomSchema>;
+
 const RoomList = ({ rooms }: RoomListProps) => {
+  const router = useRouter();
+  const handleJoinRoom = async (roomId: string) => {
+    const res = await joinRoom(roomId);
+    console.log(res);
+    if (res.message === 'join success') {
+      router.push(`/room/${res.data.id}`);
+    } else if (res.message === 'room not exits') {
+      toast.error('部屋が存在しません');
+    } else if (res.message === 'room is full') {
+      toast.error('部屋が満員です');
+    }
+  };
   return (
-    <ScrollArea className='max-h-[400px]'>
+    <ScrollArea>
       <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
         {rooms.map((room) => (
           <Card key={room.id} className='transition-shadow duration-300 hover:shadow-lg'>
@@ -40,9 +64,9 @@ const RoomList = ({ rooms }: RoomListProps) => {
               </div>
             </CardContent>
             <CardFooter className='flex'>
-              <Link href={`/room/${room.id}`} className='w-full' passHref>
-                <Button className='w-full'>参加する</Button>
-              </Link>
+              <Button onClick={() => handleJoinRoom(room.id)} className='w-full'>
+                参加する
+              </Button>
             </CardFooter>
           </Card>
         ))}
